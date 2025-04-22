9ab106d595ff80c1aee796336eaf9f1fb6e6333f
Dmitriy Kochetov <dimankochetov@yandex.ru>
2025-04-22 23:41:16 +0300

    ✨ feat(nvim): updated theme and renamed file with theme for neovim

---
+++ neovim/nvim/lua/plugins/theme.lua
@@ -0,0 +1,19 @@
+-- catppuccin-frappe
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000, -- чтобы тема применялась до загрузки других плагинов
+--   config = function()
+--     vim.cmd.colorscheme "catppuccin-frappe"
+--   end
+-- }
+
+-- rose-pine
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end,
+}
