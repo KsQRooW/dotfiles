c0042726b15f604b693702a2f687993e71591b51
Dmitriy Kochetov <dimankochetov@yandex.ru>
2025-04-24 20:17:35 +0300

    ✨ feat: updated PowerShell profile - added aliases

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,5 @@
-clear
 oh-my-posh init pwsh --config "$env:DOTFILES/oh-my-posh/catppuccin_ksq.omp.json" | Invoke-Expression
+
+# functions (like aliases)
+function lg { lazygit }
+function cdd { cd D:\dotfiles }
