89c21939c44db628c556b61e0c195cb53f36b0e2
Dmitriy Kochetov <dimankochetov@yandex.ru>
2025-04-26 14:19:26 +0300

    ✨ feat: zenmode with left/right md files

--- neovim/nvim/lua/plugins/zenmode.lua
+++ neovim/nvim/lua/plugins/zenmode.lua
@@ -3,15 +3,6 @@ return {
 	config = function()
 		require("no-neck-pain").setup({
 			width = 120,
-			buffers = {
-				scratchPad = {
-					enabled = true,
-					location = nil,
-				},
-				bo = {
-					filetype = "md",
-				},
-			},
 		})
 	end,
 }
