ef91d9918060665211e973cb5df8012d2266662a
Dmitriy Kochetov <dimankochetov@yandex.ru>
2025-05-30 00:13:53 +0300

    ✨ feat: added mypy, black to nonels for formatting and type checking in python + refactored format/lint tools loading

--- neovim/nvim/lua/plugins/nonels.lua
+++ neovim/nvim/lua/plugins/nonels.lua
@@ -4,6 +4,18 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 
+		local venv_path = os.getenv("VIRTUAL_ENV")
+		local is_windows = package.config:sub(1, 1) == "\\"
+		local python_bin = nil
+
+		if venv_path then
+			if is_windows then
+				python_bin = venv_path .. "\\Scripts"
+			else
+				python_bin = venv_path .. "/bin"
+			end
+		end
+
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.prettier.with({
@@ -20,8 +32,21 @@ return {
 					},
 				}),
 
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.dart_format,
+				null_ls.builtins.formatting.stylua.with({
+					filetypes = { "lua" },
+				}),
+				null_ls.builtins.formatting.dart_format.with({
+					filetypes = { "dart" },
+				}),
+				null_ls.builtins.formatting.black.with({
+					command = python_bin and (python_bin .. (is_windows and "\\black.exe" or "/black")),
+					filetypes = { "python" },
+				}),
+				null_ls.builtins.diagnostics.mypy.with({
+					command = python_bin and (python_bin .. (is_windows and "\\mypy.exe" or "/mypy")),
+					filetypes = { "python" },
+				}),
+				-- ruff configured in lsp.lua
 			},
 
 			-- autoformat on save
