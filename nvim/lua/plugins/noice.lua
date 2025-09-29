3790ba3a169a3e6d51985f0996eae7fedc3e82ac
Dmitriy Kochetov <dimankochetov@yandex.ru>
2025-09-29 10:01:24 +0300

    ✨ feat: add noice plugin

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    presets = {
+      command_palette = true,
+      bottom_search = true,
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+}
