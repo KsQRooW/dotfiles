e206526e6eaf4ee7632137f85fa1e8f6b315b95a
Dmitriy Kochetov <dimankochetov@yandex.ru>
2025-10-25 16:50:34 +0300

    ✨ feat: create my own kanagawa theme

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:DOTFILES/oh-my-posh/catppuccin_ksq.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:DOTFILES/oh-my-posh/ksq_kanagawa.omp.json" | Invoke-Expression
 
 # functions (like aliases)
 function lg { lazygit }
