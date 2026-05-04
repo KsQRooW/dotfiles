380fe02eb8ce1971524198412beb44ff626d93f4
Dmitriy Kochetov <dimankochetov@yandex.ru>
2026-05-04 16:09:21 +0300

    ✨ feat: update powershell configs

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,6 @@ oh-my-posh init pwsh --config "$env:DOTFILES/oh-my-posh/ksq_kanagawa.omp.json" |
 # functions (like aliases)
 function lg { lazygit }
 function cdd { cd D:\dotfiles }
+function todo { nvim D:\dotfiles\notes\todo.md }
+function activate { ./venv/Scripts/activate.ps1 }
+function install-dudes { uv pip install debugpy pytest-language-server mypy ruff black deptry }
