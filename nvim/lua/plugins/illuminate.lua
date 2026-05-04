cd25f7cd00370888b5c4c5b33123f1e7dfed7c97
Dmitriy Kochetov <dimankochetov@yandex.ru>
2026-05-04 16:16:09 +0300

    ✨ feat(nvim): add plugin for highlighting word under the cursor

---
+++ nvim/lua/plugins/illuminate.lua
@@ -0,0 +1,8 @@
+return {
+  "RRethy/vim-illuminate",
+  config = function()
+    -- vim.cmd("hi IlluminatedWordText guibg=#1b1c23 gui=NONE cterm=NONE")
+    -- vim.cmd("hi IlluminatedWordRead guibg=#1b1c23 gui=NONE cterm=NONE")
+    -- vim.cmd("hi IlluminatedWordWrite guibg=#1b1c23 gui=NONE cterm=NONE")
+  end,
+}
