7858d0591eab5fc236ef3ef949d5713cdb0f2aa2
Dmitriy Kochetov <dimankochetov@yandex.ru>
2026-05-04 16:14:45 +0300

    ✨ feat(nvim): supporting vscode launch.json files + little changes for dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -10,13 +10,14 @@ return {
       local dap, dapui = require("dap"), require("dapui")
       dapui.setup()
       require("dap-python").setup("python")
+      require("dap.ext.vscode").load_launchjs()
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
+      -- dap.listeners.before.attach.dapui_config = function()
+      --   dapui.open()
+      -- end
+      -- dap.listeners.before.launch.dapui_config = function()
+      --   dapui.open()
+      -- end
       dap.listeners.before.event_terminated.dapui_config = function()
         dapui.float_element("console", { position = "center", height = 20, width = 120 })
       end
@@ -30,9 +31,9 @@ return {
           request = "launch",
           name = "current file",
           program = "${file}",
-          pythonPath = function()
-            return "python"
-          end,
+          -- pythonPath = function()
+          --   return "python"
+          -- end,
           env = {
             PYTHONPATH = "${workspaceFolder}",
           },
@@ -44,10 +45,11 @@ return {
           request = "launch",
           name = "pytest",
           module = "pytest",
-          pythonPath = function()
-            return "python"
-          end,
+          -- pythonPath = function()
+          --   return "python"
+          -- end,
           args = {
+            "main.py",
             "-x",
             "--log-level=INFO",
           },
