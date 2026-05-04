8407392f684bfba49192764e4151983366d7866f
Dmitriy Kochetov <dimankochetov@yandex.ru>
2026-05-04 16:20:41 +0300

    ✨ feat(nvim): add navic plugin + add codes tree position into lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,20 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup({})
+    local navic = require("nvim-navic")
+    require("lualine").setup({
+      winbar = {
+        lualine_c = {
+          {
+            function()
+              return navic.get_location()
+            end,
+            cond = function()
+              return navic.is_available()
+            end,
+          },
+        },
+      },
+    })
   end,
 }
---
+++ nvim/lua/plugins/navic.lua
@@ -0,0 +1,12 @@
+return {
+  "SmiteshP/nvim-navic",
+  config = function()
+    require("nvim-navic").setup({
+      lsp = {
+        auto_attach = true,
+        preference = { "pyright" },
+      },
+      highlight = true,
+    })
+  end,
+}
